56d23e3173b0bbfd7a5fe2891bc7c55b5e24ec25
qw1234-qw <157669388+qw1234-qw@users.noreply.github.com>
2024-09-29 17:28:17 +0800

    更新 script.js

--- script.js
+++ script.js
@@ -1,41 +1,30 @@
-function floatingAlert() {
-      const alertBox = document.getElementById('floatingAlert');
-      alertBox.innerText.display = 'block'; // 显示提示框
-      setTimeout(() => {
-        alertBox.style.display = 'none'; // 3秒后隐藏提示框
-      }, 3000);
-    }
+function showAlert(message) {
+    const alertBox = document.getElementById('floatingAlert');
+    alertBox.innerText = message;
+    alertBox.style.display = 'block';
+    setTimeout(() => {
+        alertBox.style.display = 'none';
+    }, 3000);
+}
 
-    // 页面加载时显示加载图像
-    window.onload = function() {
-      showAlert();
-      const loadingScreen = document.getElementById('loading');
-      setTimeout(() => {
-        loadingScreen.style.display = 'none'; // 3秒后隐藏加载图像
-      }, 4000);
-    };
-// 显示软件加载屏幕
+window.onload = function() {
+    showAlert('XX制作，仅用于合法用途，一切非法使用概不负责');
+    setTimeout(() => {
+        document.getElementById('loading').style.display = 'none';
+    }, 4000);
+};
+
 function showLoadingScreen() {
-    const loadingScreen = document.getElementById('loading'); // 获取加载屏幕元素
-    loadingScreen.style.display = 'block'; // 显示加载屏幕
+    document.getElementById('loading').style.display = 'flex';
 }
 
-// 隐藏软件加载屏幕
 function hideLoadingScreen() {
-    const loadingScreen = document.getElementById('loading'); // 获取加载屏幕元素
-    loadingScreen.style.display = 'none'; // 隐藏加载屏幕
+    document.getElementById('loading').style.display = 'none';
 }
- 
-   hideLoadingScreen()
 
-
-
-// 导航到对应页面
 function navigateToPage(buttonNumber) {
-    
     let url;
 
-    // 根据按钮号设置子页面的URL
     switch (buttonNumber) {
         case 1:
             url = "https://dyfllnqeesez.xyz/vod/list.html?type_id=1043";
@@ -56,17 +45,14 @@ function navigateToPage(buttonNumber) {
             url = "https://v6v3265.xyz/";
             break;
         default:
-            url = "indeed.html"; // 默认页面URL
+            url = "index.html";
     }
-    floatingAlert('加载中...');
-    showLoadingScreen(); // 显示加载中
-    window.location.href = url;
-    
 
-    // 在这里隐藏加载屏幕，延迟4秒后导航
+    showLoadingScreen();
+    showAlert('加载中...');
+
     setTimeout(() => {
-        hideLoadingScreen(); // 隐藏加载屏幕
-         // 导航到指定页面
-    }, 99999999000); 
+        hideLoadingScreen();
+        window.location.href = url;
+    }, 2000);
 }
-   window.onload = () => showAlert('XX制作，仅用于合法用途，一切非法使用概不负责');
