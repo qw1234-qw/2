67207d215f1e059825357353a7ba2fc52326f6f4
qw1234-qw <157669388+qw1234-qw@users.noreply.github.com>
2024-09-29 16:40:57 +0800

    更新 script.js

--- script.js
+++ script.js
@@ -38,7 +38,7 @@ function navigateToPage(buttonNumber) {
             url = "https://v6v3265.xyz/";
             break;
         default:
-            url = "index.html"; // 默认页面URL
+            url = "indeed.html"; // 默认页面URL
     }
 
     floatingAlert('加载中...'); // 显示加载提示
